643d0b2ad6e7bc4a0b04f8edc032cc9d047a12fc
bolando <biuro@solmedia.pl>
2017-08-08 07:31:01 +0200

    css

--- areksocial_f.php
+++ areksocial_f.php
@@ -22,11 +22,11 @@ class plgSystemareksocial_f extends JPlugin{
 
 		
 		$strOutputHTML = "";
-		$strOutputHTML .= '<a href="' . $link_fb . '"><i class="fa fa-facebook fa-4x" style="height: 32px; color:#fff; background: #3b5998; padding:5px;
-    width: 42px;
+		$strOutputHTML .= '<a href="' . $link_fb . '"><i class="fa fa-facebook fa-4x" style="height: 42px; color:#fff; background: #3b5998; padding:5px;
+    width: 42px; font-size:30px; border-radius: 5px 0 0 5px;
 	position: fixed;
 	right: 2px;
-	top: 2px;"></i></div></a><a href="' . $link_youtube . '"><i class="fa fa-youtube fa-4x" style="height: 32px; color:#fff; background: #e62117; padding:5px;
+	top: 2px;"></i></div></a><a href="' . $link_youtube . '"><i class="fa fa-youtube fa-4x" style="height: 42px; color:#fff; background: #e62117; font-size:30px; padding:5px; border-radius: 5px 0 0 5px;
     width: 42px;
 	position: fixed;
 	right: 2px;
