bb9cddcee4d461d78c690b5ec7371aeefef51a93
bolando <biuro@solmedia.pl>
2017-08-08 07:33:11 +0200

    css3

--- areksocial_f.php
+++ areksocial_f.php
@@ -23,14 +23,14 @@ class plgSystemareksocial_f extends JPlugin{
 		
 		$strOutputHTML = "";
 		$strOutputHTML .= '<a href="' . $link_fb . '"><i class="fa fa-facebook fa-4x" style="height: 42px; color:#fff; background: #3b5998; padding:5px;
-    width: 42px; font-size:30px; border-radius: 5px 0 0 5px;
+    width: 42px; font-size:30px; border-radius: 5px 0 0 5px; text-align:center;
 	position: fixed;
-	right: 2px;
-	top: 2px;"></i></div></a><a href="' . $link_youtube . '"><i class="fa fa-youtube fa-4x" style="height: 42px; color:#fff; background: #e62117; font-size:30px; padding:5px; border-radius: 5px 0 0 5px;
+	right: 0;
+	top: 52px;"></i></div></a><a href="' . $link_youtube . '"><i class="fa fa-youtube fa-4x" style="height: 42px; color:#fff; background: #e62117; font-size:30px; padding:5px; border-radius: 5px 0 0 5px;text-align:center;
     width: 42px;
 	position: fixed;
-	right: 2px;
-	top: 56px;"></i></div></a>';
+	right: 0;
+	top: 100px;"></i></div></a>';
 		
 		$body = $app->getBody();
 		if ($app->isSite() == false)
