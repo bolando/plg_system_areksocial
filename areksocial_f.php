a59fa5208830cdc5da92a5721e1f62810d408b50
Andrzej Bolewski <Andrzej Bolewski>
2018-02-16 07:27:15 +0100

    2018

--- areksocial_f.php
+++ areksocial_f.php
@@ -10,14 +10,16 @@ class plgSystemareksocial_f extends JPlugin{
 	{
 		
 		$CSS_FOLDER = JURI::root() . 'plugins/system/areksocial_f/addons/';
-		
-		$areksocialcss = '<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">'.'\n <link type="text/css" rel="stylesheet" href= "' . $CSS_FOLDER . 'style.css" />';
+		$areksocialcss ='<link type="text/css" rel="stylesheet" href= "' . $CSS_FOLDER . 'style.css" />';
+		if ($this->params->get('awesome','') == '1')
+			{$areksocialcss .= '<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">';}
 	
 
 		
 		$app = JFactory::getApplication();
 		$link_fb = $this->params->get('link_fb','');
 		$link_youtube = $this->params->get('link_youtube','');
+		$link_insta = $this->params->get('link_insta','');
 		$document = JFactory::getDocument();
 
 		
@@ -25,6 +27,7 @@ class plgSystemareksocial_f extends JPlugin{
 		$strOutputHTML .= '<div class="aa-social">';
 		if ($link_fb != '') {$strOutputHTML .= '<a target="_blank" href="' . $link_fb . '"><i class="fa fa-facebook " ></i></a>';};
 		if ($link_youtube != '') {$strOutputHTML .= '<a target="_blank" href="' . $link_youtube . '"><i class="fa fa-youtube" ></i></a>';};
+		if ($link_insta != '') {$strOutputHTML .= '<a target="_blank" href="' . $link_insta . '"><i class="fa fa-instagram" ></i></a>';};
 		$strOutputHTML .= '</div>';
 		
 		$body = $app->getBody();
